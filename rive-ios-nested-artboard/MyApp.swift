8d53fb8a772d6417f3a62035085009ec6f6024ea
Waj <agentwaj@gmail.com>
2025-05-07 15:40:22 -0400

    Add broken demo

--- rive-ios-nested-artboard/MyApp.swift
+++ rive-ios-nested-artboard/MyApp.swift
@@ -1,3 +1,4 @@
+import RiveRuntime
 import SwiftUI
 
 @main
@@ -10,8 +11,30 @@ struct MyApp: App {
 }
 
 struct ContentView: View {
+    @State var parentVm: RiveViewModel
+    @State var nestedVm: RiveViewModel
+
+    init() {
+        parentVm = RiveViewModel(
+            fileName: "rive-ios-nested-artboard",
+            artboardName: "parent"
+        )
+
+        nestedVm = RiveViewModel(
+            fileName: "rive-ios-nested-artboard",
+            artboardName: "nested"
+        )
+    }
+
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            parentVm.view()
+            nestedVm.view()
+        }
+        .onAppear {
+            parentVm.setInput("num_input", value: 50.0, path: "nested")
+            nestedVm.setInput("num_input", value: 50.0)
+        }
     }
 }
 
