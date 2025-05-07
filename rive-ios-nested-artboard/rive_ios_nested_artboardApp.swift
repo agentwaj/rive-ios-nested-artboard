74272d34ee44dd692cc0999952af28f315164119
Waj <agentwaj@gmail.com>
2025-05-07 13:10:20 -0400

    Initial Commit

---
+++ rive-ios-nested-artboard/rive_ios_nested_artboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  rive_ios_nested_artboardApp.swift
+//  rive-ios-nested-artboard
+//
+//  Created by agentwaj on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct rive_ios_nested_artboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ rive-ios-nested-artboardTests/rive_ios_nested_artboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  rive_ios_nested_artboardTests.swift
+//  rive-ios-nested-artboardTests
+//
+//  Created by agentwaj on 5/7/25.
+//
+
+import Testing
+@testable import rive_ios_nested_artboard
+
+struct rive_ios_nested_artboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
